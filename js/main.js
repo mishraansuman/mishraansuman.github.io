ab0091f5ab83fba398fea580daeef5a3fecfca5e
ansmis-lit <ansmis.git@gmail.com>
2021-02-08 12:13:22 +0530

    v-feb2021

--- js/main.js
+++ js/main.js
@@ -64,41 +64,7 @@
 	});	
 
 
-	/*----------------------------------------------------- */
-	/* Stat Counter
-  	------------------------------------------------------- */
-   var statSection = $("#stats"),
-       stats = $(".stat-count");
-
-   statSection.waypoint({
-
-   	handler: function(direction) {
-
-      	if (direction === "down") {       		
-
-			   stats.each(function () {
-				   var $this = $(this);
-
-				   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-				   	duration: 4000,
-				   	easing: 'swing',
-				   	step: function (curValue) {
-				      	$this.text(Math.ceil(curValue));
-				    	}
-				  	});
-				});
-
-       	} 
-
-       	// trigger once only
-       	this.destroy();      	
-
-		},
-			
-		offset: "90%"
 	
-	});	
-
 
 	/*---------------------------------------------------- */
 	/*	Masonry
