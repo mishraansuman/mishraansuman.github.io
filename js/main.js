d0bbc45458360b27c4da92be9417d9ec90e6acf0
ANSUMAN MISHRA <ansmis.lit@gmail.com>
2021-05-17 23:19:45 +0530

    featureUP

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 function menuToggle(){
 	document.getElementById("checkMenu").checked=false;
 }
+/****** Scroll Reveal ********
+ Function 'reveal' */
+window.addEventListener('scroll', reveal);
+
+    function reveal(){
+      var reveals = document.querySelectorAll('.reveal');
+
+      for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 50;
+
+        if(revealtop < windowheight - revealpoint){
+          reveals[i].classList.add('active');
+        }
+        else{
+          reveals[i].classList.remove('active');
+        }
+      }
+    }
