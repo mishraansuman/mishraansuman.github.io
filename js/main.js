e23e7a984e0b1654df32bba719c57ef8ad2fcbf9
ANSUMAN MISHRA <ansmis.lit@gmail.com>
2023-07-05 20:57:49 +0530

    404 added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+// Execute the code when the window finishes loading
 window.onload = () => {
-    swal({
-      text: "'Project Section' is under development, will be updated fully in few hours.",
-      buttons: "I understand",
-    });
+  // Show a SweetAlert notification to inform the user
+  swal({
+    text: "'Project Section' is under development, will be updated fully in few hours.",
+    buttons: "I understand",
+  });
 };
+document.querySelector('.contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    // Redirect to the custom submit.html page
+    window.location.href = '/submit.html';
+  });
