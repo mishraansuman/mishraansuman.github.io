c325a20dacdbdf152bc31c4dc27cf1c24e14bd7b
ANSUMAN MISHRA <ansmis.lit@gmail.com>
2023-11-21 16:49:32 +0530

    up-21-11-2023-16:29

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// Execute the code when the window finishes loading
-window.onload = () => {
-  // Show a SweetAlert notification to inform the user
-  swal({
-    text: "'Project Section' is under development, will be updated fully in few hours.",
-    buttons: "I understand",
-  });
-};
+// // Execute the code when the window finishes loading
+// window.onload = () => {
+//   // Show a SweetAlert notification to inform the user
+//   swal({
+//     text: "'Project Section' is under development, will be updated fully in few hours.",
+//     buttons: "I understand",
+//   });
+// };
